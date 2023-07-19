6b419bd7b8d3c4a90e2af9789e44083fc9434f8a
DonJanus <113478479+DonJanus@users.noreply.github.com>
2023-07-19 07:52:11 -0300

    Create verificando_servico_apache2.sh

---
+++ verificando_servico_apache2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+resposta_http=$(curl --write-out %{http_code} --silent --output /dev/null http://localhost)
+if [ $resposta_http -eq 200 ]
+then 
+	echo "Servidor online!"
+else
+	echo "Houve um problema com o servidor. Tentando reiniciar"
+	systemctl restart apache2
+	echo "...Servidor reinicializando"
+fi
