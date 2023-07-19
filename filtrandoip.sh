b4675c643b450a5c3d11e33c96661ba8d8d5e651
DonJanus <113478479+DonJanus@users.noreply.github.com>
2023-07-19 07:50:58 -0300

    Create filtrandoip.sh

---
+++ filtrandoip.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd ~/apache-log
+
+reges="\b([0-9]{1,3}\.){3}[0-9]{1,3}\b)"
+
+if [[ $1 =~ $regex ]]
+then
+
+	cat apache.log | grep $1
+	if [ $? -ne 0 ]
+	then
+		echo "Endereco ip nao encontrado"
+	fi
+else
+	echo "formato invalido"
+fi
