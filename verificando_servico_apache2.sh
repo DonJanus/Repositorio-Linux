2121952981a81a6a7c61feba5da7087c3345cf08
DonJanus <113478479+DonJanus@users.noreply.github.com>
2023-07-19 08:55:48 -0300

    Update verificando_servico_apache2.sh

--- verificando_servico_apache2.sh
+++ verificando_servico_apache2.sh
@@ -2,6 +2,7 @@
 
 resposta_http=$(curl --write-out %{http_code} --silent --output /dev/null http://localhost)
 if [ $resposta_http -eq 200 ]
+#Funcao -eq so compara valores inteiros, em caso de string utilizar "=="
 then 
 	echo "Servidor online!"
 else
